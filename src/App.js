16d724a520e3319069f67792330bdabcd70f143e
hokage <hokageqqw@yahoo.com>
2025-01-13 11:22:13 +0200

    addProduct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 function App() {
   const [products, setProducts] = useState([]);
+    const idRef = useRef();
+    const nameRef = useRef();
+    const priceRef = useRef();
+    const activeRef = useRef();
 
     useEffect(() => {
       fetch("http://localhost:8080/products")
@@ -18,18 +22,33 @@ function App() {
 
     }
 
-  return (
-      <div className="App">
-        {products.map((product, index) => (
-            <div key={product.id}>
-              <div>{product.id}</div>
-              <div>{product.name}</div>
-              <div>{product.price}</div>
-                <button onClick={() => removeProduct(index)}>Delete</button>
-            </div>
-        ))}
-      </div>
-  );
-}
+    function addProduct() {
+        fetch(
+            `http://localhost:8080/add-product/${Number(idRef.current.value)}/${nameRef.current.value}/${Number(priceRef.current.value)}/${activeRef.current.checked}`
+        )
 
-  export default App;
+        return (
+            <div className="App">
+                <label>ID</label> <br />
+                <input ref={idRef} type="number" /> <br />
+                <label>Name</label> <br />
+                <input ref={nameRef} type="text" /> <br />
+                <label>Price</label> <br />
+                <input ref={priceRef} type="number" /> <br />
+                <label>Active</label> <br />
+                <input ref={activeRef} type="checkbox" /> <br />
+                <button onClick={addProduct}>Add</button>
+                {products.map((product, index) => (
+                    <div>
+                        <div>{product.id}</div>
+                        <div>{product.id}</div>
+                        <div>{product.name}</div>
+                        <div>{product.price}</div>
+                        <button onClick={() => removeProduct(index)}>Delete</button>
+
+                    </div>)}
+            </div>
+        );
+    }
+
+        export default App;
